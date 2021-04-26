a33ee84b47354adf5d71f28a4dfaf3cc7681bf43
vishal <vishalvikaslokam@gmail.com>
2021-04-26 23:48:05 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, url_for, request
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import svm
+import joblib
+import pickle
+
+# load the model from disk
+filename = 'model.pkl'
+clf = pickle.load(open(filename, 'rb'))
+cv = pickle.load(open('transform.pkl', 'rb'))
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        vect = cv.transform(data).toarray()
+        my_prediction = clf.predict(vect)
+    return render_template('result.html', prediction=my_prediction)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
